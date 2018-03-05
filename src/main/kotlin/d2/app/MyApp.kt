b9b6e441751e1ebea4615b7d1d1e73ac8bcda5eb
daejoon.ko <kkode1911@gmail.com>
2018-03-05 21:40:59 +0900

    initial commit

---
+++ src/main/kotlin/d2/app/MyApp.kt
@@ -0,0 +1,6 @@
+package d2.app
+
+import d2.view.MainView
+import tornadofx.App
+
+class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/d2/view/MainView.kt
@@ -0,0 +1,12 @@
+package d2.view
+
+import d2.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
